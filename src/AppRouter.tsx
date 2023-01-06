22acb995c455715a91a7ad48517fbe36b2debd33
JaeyeongKim <ddi03231@g.skku.edu>
2023-01-06 23:09:52 +0900

    초안페이지 Routin완료

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
+import TopPickPage from "./pages/TopPickPage";
+import PortfolioPage from "./pages/PortfolioPage";
 import { useRecoilValue } from "recoil";
 import { isLoggedInState } from "./atoms/LoginAtom";
 
@@ -10,6 +12,8 @@ function AppRouter() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
+                <Route path="/toppick" element={<LoginPage />} />
+                <Route path="/portfolio" element={<LoginPage />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/PortfolioPage.tsx
@@ -0,0 +1,16 @@
+
+import styled from "styled-components";
+import {useState} from "react";
+
+const PortfolioPageContainer = styled.div`
+    backgorund-color = black;
+`
+
+function PortfolioPage(){
+    return(
+        <PortfolioPageContainer></PortfolioPageContainer>
+    );
+}
+
+
+export default PortfolioPage;
